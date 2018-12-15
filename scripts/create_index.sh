32f6075f2d437b2e2f99970275d87a98255c1799
Saurav Sharma <appdroiddeveloper@gmail.com>
2018-12-15 14:47:34 +0545

    try moving all script to deploy

--- scripts/create_index.sh
+++ scripts/create_index.sh
@@ -1,1 +1,1 @@
-echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html
+echo "<meta http-equiv=refresh content=0;url=`echo $TRAVIS_REPO_SLUG | cut -d '/' -f 2`/index.html>" > target/doc/index.html;
