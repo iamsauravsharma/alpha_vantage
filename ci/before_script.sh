31c6bfe653c95958fe7b9121b9ee95256fa64426
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-10-05 11:46:36 +0545

    ci: fix clippy added if statment
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- ci/before_script.sh
+++ ci/before_script.sh
@@ -5,7 +5,7 @@ if [[ "$RUSTFMT_ADDED" == "false" ]]; then
   rustup default nightly-${LAST_AVAILABLE}
   rustup component add rustfmt
 fi
-if [[ "$CLIPPY_ADDED" == "true" ]]; then
+if [[ "$CLIPPY_ADDED" == "false" ]]; then
   LAST_AVAILABLE=$(curl https://rust-lang.github.io/rustup-components-history/x86_64-apple-darwin/clippy)
   rustup toolchain install nightly-${LAST_AVAILABLE}
   rustup default nightly-${LAST_AVAILABLE}
