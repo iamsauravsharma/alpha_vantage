db0058000b65b14d637b14861d331ec789f84807
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-10-09 18:06:22 +0545

    ci: add initial cross compilation
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- ci/install.sh
+++ ci/install.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
-rustup component add rustfmt && export RUSTFMT_ADDED="true" || export RUSTFMT_ADDED="false"
-rustup component add clippy && export CLIPPY_ADDED="true" || export CLIPPY_ADDED="false"
+set -e
+
+# if $TARGET env is present install environment else install rustfmt and clippy
+if [[ -n $TARGET ]]
+then
+    rustup target add "$TARGET"
+else
+    # add rustfmt and clippy to target
+    rustup component add rustfmt && export RUSTFMT_ADDED="true" || export RUSTFMT_ADDED="false"
+    rustup component add clippy && export CLIPPY_ADDED="true" || export CLIPPY_ADDED="false"
+fi
--- ci/script.sh
+++ ci/script.sh
@@ -1,17 +1,30 @@
 #!/usr/bin/env bash/
 set -e
-if [[ $RUSTFMT_ADDED == "false" ]]
-then
-  cargo +nightly-"${LAST_AVAILABLE_FMT}" fmt --all -- --check
-else
-  cargo fmt --all -- --check
-fi
-if [[ $CLIPPY_ADDED == "false" ]]
-then
-  cargo +nightly-"${LAST_AVAILABLE_CLIPPY}" clippy --all -- -D warnings
-else
+
+run_all_cargo_command(){
+  if [[ $RUSTFMT_ADDED == "false" ]]
+  then
+    cargo +nightly-"${LAST_AVAILABLE_FMT}" fmt --all -- --check
+  else
+    cargo fmt --all -- --check
+  fi
+
+  if [[ $CLIPPY_ADDED == "false" ]]
+  then
+    cargo +nightly-"${LAST_AVAILABLE_CLIPPY}" clippy --all -- -D warnings
+  else
     cargo clippy --all -- -D warnings
+  fi
+
+  cargo build --all
+  cargo doc --no-deps --all
+  cargo test --all
+}
+
+# if $TARGET is present then run only build over that target else run_all_cargo_command
+if [[ -n $TARGET ]]
+then
+  cargo build --all --target="$TARGET"
+else
+  run_all_cargo_command
 fi
-cargo build --all
-cargo doc --no-deps --all
-cargo test --all
