a82df7b07ac5c061e3bf88d2c72c2cb0938c6b7b
Saurav Sharma <appdroiddeveloper@gmail.com>
2019-10-10 17:09:29 +0545

    feat: add vendored feature and build on musl target
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- ci/install.sh
+++ ci/install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+#shellcheck disable=SC2015
+#SC2015 => Note that A && B || C is not if-then-else. C may run when A is true.
+#SC2015 => https://github.com/koalaman/shellcheck/wiki/SC2015
 set -e
 
 # if $TARGET env is present install cross else install rustfmt and clippy
--- ci/script.sh
+++ ci/script.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash/
+#shellcheck disable=SC2086
+#SC2086 => Double quote to prevent globbing and word splitting
+#SC2086 => https://github.com/koalaman/shellcheck/wiki/SC2086
 set -e
 
 run_all_cargo_command(){
   if [[ $RUSTFMT_ADDED == "false" ]]
   then
-    cargo +nightly-"${LAST_AVAILABLE_FMT}" fmt --all -- --check
+    cargo +nightly-"${LAST_AVAILABLE_FMT}" fmt --all $FEATURES -- --check
   else
-    cargo fmt --all -- --check
+    cargo fmt --all $FEATURES -- --check
   fi
 
   if [[ $CLIPPY_ADDED == "false" ]]
   then
-    cargo +nightly-"${LAST_AVAILABLE_CLIPPY}" clippy --all -- -D warnings
+    cargo +nightly-"${LAST_AVAILABLE_CLIPPY}" clippy --all $FEATURES -- -D warnings
   else
-    cargo clippy --all -- -D warnings
+    cargo clippy --all $FEATURES -- -D warnings
   fi
 
-  cargo build --all
-  cargo doc --no-deps --all
-  cargo test --all
+  cargo build --all $FEATURES
+  cargo doc --no-deps --all $FEATURES
+  cargo test --all $FEATURES
 }
 
 # if $TARGET is present then run only build over that target else run_all_cargo_command
 if [[ -n $TARGET ]]
 then
-    cargo build --all --target="$TARGET"
+    cargo build --all --target="$TARGET" $FEATURES
 else
   run_all_cargo_command
 fi
