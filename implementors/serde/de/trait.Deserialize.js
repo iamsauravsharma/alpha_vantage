12e7c93aa7a079fc40015267099fde00f333f4f1
Saurav Sharma <appdroiddeveloper@gmail.com>
2020-04-05 06:54:58 +0000

    Deploy iamsauravsharma/alpha_vantage to github.com/iamsauravsharma/alpha_vantage.git:gh-pages

--- implementors/serde/de/trait.Deserialize.js
+++ implementors/serde/de/trait.Deserialize.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
-implementors["alpha_vantage"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.104/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"alpha_vantage/search/struct.DataValue.html\" title=\"struct alpha_vantage::search::DataValue\">DataValue</a>","synthetic":false,"types":["alpha_vantage::search::DataValue"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+implementors["alpha_vantage"] = [{"text":"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.106/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"alpha_vantage/search/struct.DataValue.html\" title=\"struct alpha_vantage::search::DataValue\">DataValue</a>","synthetic":false,"types":["alpha_vantage::search::DataValue"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
