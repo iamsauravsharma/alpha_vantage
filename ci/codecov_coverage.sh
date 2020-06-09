66dcd4fcaeec7a80dcbc6c40292eac7a306d96d1
Saurav Sharma <appdroiddeveloper@gmail.com>
2020-06-09 18:30:41 +0545

    ci: speed cargo tarpaulin install
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- ci/codecov_coverage.sh
+++ ci/codecov_coverage.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-cargo install cargo-tarpaulin
+bash <(curl https://raw.githubusercontent.com/xd009642/tarpaulin/master/travis-install.sh)
 cargo tarpaulin --all-features --verbose --run-types Tests Doctests --out Xml
 bash <(curl -s https://codecov.io/bash)
 
