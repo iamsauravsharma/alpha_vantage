41c59ca7b2ac7cac8674112d47e8fc04d0d887a8
iamsauravsharma <appdroiddeveloper@gmail.com>
2021-01-22 09:51:07 +0000

    Deploying to gh-pages from  @ a58cc65314e11300ff4186af173137f11ea0c0f1 🚀

---
+++ alpha_vantage/earning/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Annual","Struct to store information of annual earning"],["Earning","Struct to store earning for symbol"],["Quarterly","Struct to store information of quarterly earning"]]});
--- implementors/serde/de/trait.Deserialize.js
+++ implementors/serde/de/trait.Deserialize.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["alpha_vantage"] = [{"text":"impl&lt;'de&gt; Deserialize&lt;'de&gt; for DataValue","synthetic":false,"types":[]}];
+implementors["alpha_vantage"] = [{"text":"impl&lt;'de&gt; Deserialize&lt;'de&gt; for Annual","synthetic":false,"types":[]},{"text":"impl&lt;'de&gt; Deserialize&lt;'de&gt; for Quarterly","synthetic":false,"types":[]},{"text":"impl&lt;'de&gt; Deserialize&lt;'de&gt; for DataValue","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
