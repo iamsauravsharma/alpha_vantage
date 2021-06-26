cc54348428a0b6a6ba92e66c81ec35e0342b6889
iamsauravsharma <appdroiddeveloper@gmail.com>
2021-06-26 13:16:20 +0000

    Deploying to gh-pages from  @ b269d66f12ba6be8bc0dbbb88cca9fe652ad052c 🚀

--- alpha_vantage/crypto_rating/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"struct":[["CryptoRating","Struct used for health index rating"]]});
--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["alpha_vantage"] = {"name":"","files":["api.rs","client.rs","crypto.rs","crypto_rating.rs","custom.rs","deserialize.rs","earning.rs","error.rs","exchange.rs","forex.rs","lib.rs","quote.rs","search.rs","sector.rs","stock_time.rs","technical_indicator.rs","utils.rs"]};
+sourcesIndex["alpha_vantage"] = {"name":"","files":["api.rs","client.rs","crypto.rs","custom.rs","deserialize.rs","earning.rs","error.rs","exchange.rs","forex.rs","lib.rs","quote.rs","search.rs","sector.rs","stock_time.rs","technical_indicator.rs","utils.rs"]};
 createSourceSidebar();
