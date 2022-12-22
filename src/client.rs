2a93147c50128391c8313280d7cbb506caf07a93
Saurav Sharma <appdroiddeveloper@gmail.com>
2022-12-22 17:00:17 +0545

    refactor(BREAKING): remove first party support for surf client
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- src/client.rs
+++ src/client.rs
@@ -5,10 +5,10 @@ use crate::error::{Error, Result};
 #[async_trait]
 /// Trait which can be implemented for all common library client for getting
 /// output from server
-/// surf and reqwest are two client which are supported with feature flag. If
+/// reqwest is client which is supported with feature flag. If
 /// you prefer alternate http client you can add support by implementing
 /// `HttpClient` trait for client.
-/// Some example of other client which can be used are isahc client
+/// Some example of other client which can be used are surf and isahc client
 pub trait HttpClient {
     /// AlphaVantage provider output function which provides one field path
     /// where get GET request needs to be performed
@@ -42,26 +42,6 @@ impl HttpClient for reqwest::Client {
             .await
             .map_err(|_| Error::GetRequestFailed)?
             .text()
-            .await
-            .map_err(|_| Error::GetRequestFailed)
-    }
-}
-
-#[cfg(feature = "surf-client")]
-#[async_trait]
-impl HttpClient for surf::Client {
-    async fn get_alpha_vantage_provider_output(&self, path: &str) -> Result<String> {
-        self.get(path)
-            .recv_string()
-            .await
-            .map_err(|_| Error::GetRequestFailed)
-    }
-
-    async fn get_rapid_api_provider_output(&self, path: &str, api_key: &str) -> Result<String> {
-        self.get(path)
-            .header("x-rapidapi-host", "alpha-vantage.p.rapidapi.com")
-            .header("x-rapidapi-key", api_key)
-            .recv_string()
             .await
             .map_err(|_| Error::GetRequestFailed)
     }
